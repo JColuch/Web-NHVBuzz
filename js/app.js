93dff9f6e51884105f484a21e43a292d8012e408
JoelColucci <JoelColucci@Gmail.com>
2015-01-12 23:54:59 -0500

    Cleaned up app.js and impd menu toggle in knockout

--- js/app.js
+++ js/app.js
@@ -39,6 +39,11 @@ var ViewModel = function() {
   var self = this;
 
   var $searchInput = $(".search-input")[0]; // autocomplete via Places library
+
+  var $sidebarBtn = $(".menu-toggle");
+  var $sidebar = $(".side-bar");
+  var $sidebarHeader = $(".side-bar-header");
+
   var $infoPane = $(".info-bar");
   
   var mapContainer = document.getElementById("map-canvas");
@@ -51,11 +56,10 @@ var ViewModel = function() {
 
   self.searchTerm = ko.observable();
   self.sideBarTitle = ko.observable();
-  self.placeList = ko.observableArray([]);
   self.chosenPlaceData = ko.observable();
+  self.placeList = ko.observableArray([]);
 
   self.sideBarTitle("Places");
-
 
   // Behaviors    
   self.goToPlace = function(place) { 
@@ -64,42 +68,16 @@ var ViewModel = function() {
     self.chosenPlaceData(place);
   };
 
+  self.toggleSidebar = function() {
+    $sidebarBtn.toggleClass("toggle");
+    $sidebarHeader.toggleClass("sbh-active");
+    $sidebar.toggleClass("side-bar-active");
+    $infoPane.removeClass("info-bar-active");
+  };
 
   self.closeInfoBar = function() {
     $infoPane.removeClass("info-bar-active");
-  }
-
-
-  // Operations
-
-  function initializeMap() {
-
-    var newHaven = { lat: 41.3100, lng: -72.924 };
-
-    var mapOptions = {
-      center: newHaven,
-      zoom: 14,
-      panControl: false,
-      zoomControl: false,
-      mapTypeControl: false,
-      scaleControl: false,
-      streetViewControl: false,
-      overviewMapControl: false
-    };
-
-    // Set ViewModel scoped variables
-    map = new google.maps.Map(mapContainer, mapOptions);
-    mapBounds = new google.maps.LatLngBounds();
-    infoWindow = new google.maps.InfoWindow();
-
-    var options = {
-      bounds: mapBounds,
-      types: ['establishment']
-    };
-
-    autocomplete = new google.maps.places.Autocomplete($searchInput, options);
-  } // End function initializeMap()
-
+  };
 
   self.setInfoWindow = function(data) {
     var content = "";
@@ -114,59 +92,7 @@ var ViewModel = function() {
     infoWindow.open(map);
 
     map.panTo(data.position);
-  }
-
-
-  self.createMarker = function(data) {
-    var name = data.name;
-
-    var position = data.position;
-
-    var address = data.address;
-
-    var rating = data.rating || "No rating available";
-
-    // marker is an object with additional data about the pin
-    // for a single location
-    var marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: name
-      //animation: google.maps.Animation.DROP
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      self.setInfoWindow(data);
-    });
-
-    // Keep track of markers
-    markers.push(marker);
-  }
-
-  // SOURCE: https://developers.google.com/maps/documentation/
-  // javascript/examples/marker-remove
-  // Sets the map on all markers in the array.
-  function setAllMap(map) {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-    }
-  }
-
-  // Removes the markers from the map, but keeps them in the array.
-  function clearMarkers() {
-    setAllMap(null);
-  }
-
-  // Deletes all markers in the array by removing references to them.
-  function deleteMarkers() {
-    clearMarkers();
-    markers = [];
-  }
-
-
-  // On load initalize map
-  google.maps.event.addDomListener(window, "load", initializeMap);
-
+  };
 
   self.getPlaces =function() {
     // Get search term from input
@@ -185,9 +111,11 @@ var ViewModel = function() {
     self.sideBarTitle(title);
 
     self.getFoursquareData(searchTerm);
-  }
+  };
 
 
+
+  // Operations
   function loadSideBar(data) {
     // Empty side bar
     self.placeList.removeAll();
@@ -251,10 +179,10 @@ var ViewModel = function() {
         url: venue.url || "#",
         imgUrl: tips.photourl || "",
         position: getPositionCoords(venue.location)
-      }
+      };
 
 
-      self.createMarker(place);
+      createMarker(place);
 
       foursquareData.push(place);
     }
@@ -282,152 +210,86 @@ var ViewModel = function() {
     return fullAddress;
   }
 
+
+  function initializeMap() {
+
+    var newHaven = { lat: 41.3100, lng: -72.924 };
+
+    var mapOptions = {
+      center: newHaven,
+      zoom: 14,
+      panControl: false,
+      zoomControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      overviewMapControl: false
+    };
+
+    // Set ViewModel scoped variables
+    map = new google.maps.Map(mapContainer, mapOptions);
+    mapBounds = new google.maps.LatLngBounds();
+    infoWindow = new google.maps.InfoWindow();
+
+    var options = {
+      bounds: mapBounds,
+      types: ['establishment']
+    };
+
+    autocomplete = new google.maps.places.Autocomplete($searchInput, options);
+  } // End function initializeMap()
+
+  function createMarker(data) {
+    var name = data.name;
+
+    var position = data.position;
+
+    var address = data.address;
+
+    var rating = data.rating || "No rating available";
+
+    // marker is an object with additional data about the pin
+    // for a single location
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: name
+      //animation: google.maps.Animation.DROP
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      self.setInfoWindow(data);
+    });
+
+    // Keep track of markers
+    markers.push(marker);
+  }
+
+  // SOURCE: https://developers.google.com/maps/documentation/
+  // javascript/examples/marker-remove
+  // Sets the map on all markers in the array.
+  function setAllMap(map) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+    }
+  }
+
+  // Removes the markers from the map, but keeps them in the array.
+  function clearMarkers() {
+    setAllMap(null);
+  }
+
+  // Deletes all markers in the array by removing references to them.
+  function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+  }
+
+  // On load initalize map
+  google.maps.event.addDomListener(window, "load", initializeMap);
+
 };
 
 ko.applyBindings(new ViewModel());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var sbHeader = document.getElementsByClassName("side-bar-header")[0];
-var elm = document.getElementsByClassName("menu-toggle")[0];
-var infoClose = document.getElementsByClassName("close-btn")[0];
-var bar = document.getElementsByClassName("side-bar")[0];
-var listings = document.getElementsByClassName("place-listings")[0];
-var logo = document.getElementsByClassName("fa")[0];
-var info = document.getElementsByClassName("info-bar")[0];
-
-
-elm.addEventListener('click', function() {
-  sbHeader.classList.toggle("sbh-active");
-  bar.classList.toggle("side-bar-active");
-  elm.classList.toggle("toggle");
-  info.classList.remove("info-bar-active");
-})
-
-// console.log(moreButton);
-// var moreButton = document.getElementsByClassName("place-link");
-// listings.addEventListener('click', function() {
-
-//   info.classList.add("info-bar-active");
-// });
-
-// infoClose.addEventListener('click', function() {
-
-//   info.classList.remove("info-bar-active");
-// });
-
-
-
-
-
-
-
-
-  // self.createMarker = function(placeData) {
-  //   var name = placeData.name;
-  //   // var lat = placeData.geometry.location.lat();  // latitude from the place service
-  //   // var lon = placeData.geometry.location.lng();  // longitude from the place service
-  //   var position = placeData.geometry.location;
-  //   var bounds = mapBounds;
-  //   var address = placeData.formatted_address;
-  //   var rating = placeData.rating || "No rating available";
-  //   // marker is an object with additional data about the pin
-  //   // for a single location
-  //   var marker = new google.maps.Marker({
-  //     map: map,
-  //     position: position,
-  //     title: name
-  //     //animation: google.maps.Animation.DROP
-  //   });
-
-  //   // Keep track of markers
-  //   markers.push(marker);
-
-  //   var content = '<div class="gm-title">' + name + "</h5>";
-  //   content += '<div>' + rating + '</div>';
-  //   content += '<div class="gm-addr">' + address + '</div>';
-  //   content += '<div class="gm-website"><a href="#">ynhh.org</a></div>';
-    
-  //   // Attach infowidnow
-  //   var infowindow = new google.maps.InfoWindow({
-  //     content: content
-  //   });
-
-  //   google.maps.event.addListener(marker, 'click', function() {
-  //     // Center map on marker clicked
-  //     map.panTo(marker.getPosition());
-
-  //     // Close current open window
-  //     if (openInfoWindow) {
-  //       openInfoWindow.close();
-  //     }
-
-  //     openInfoWindow = infowindow;
-  //     openInfoWindow.open(map, marker);
-  //   });
-
-  // }
-
-
-  // self.getPlaces =function() {
-  //   // Get search term from input
-  //   var searchTerm = self.searchTerm();
-  //   if (!searchTerm) {
-  //     searchTerm = "Restaurants";
-  //   }
-
-  //   // Clear input box
-  //   self.searchTerm("");
-
-  //   // Update Side Bar Title
-  //   var title = searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1);
-  //   self.sideBarTitle(title);
-
-  //   // var service = new google.maps.places.PlacesService(map);
-    
-  //   // var request = {
-  //   //   location: { lat: 41.3100, lng: -72.924 },
-  //   //   radius: '300',
-  //   //   query: searchTerm
-  //   // };
-
-  //   // service.textSearch(request, self.callback);
-
-  //   self.getFoursquareData(searchTerm);
-  // }
-
-  // // Source: https://developers.google.com/maps/documentation/javascript/
-  // // places#TextSearchRequests
-  // self.callback = function(results, status) {
-  //   if (status == google.maps.places.PlacesServiceStatus.OK) {
-  //     // Clear old markers
-  //     deleteMarkers();
-
-  //     for (var i = 0; i < results.length; i++) {
-  //       self.createMarker(results[i]);
-  //     }
-
-  //     //loadSideBar(results);
-  //   }
-  //   else {
-  //     console.log("ERROR");
-  //   }
-  // }
-
 
 
